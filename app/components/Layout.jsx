8d6a7dec665f9c07da8ec82767bc609a4ad16c68
MiriamHerrera <53792107+MiriamHerrera@users.noreply.github.com>
2025-05-17 12:07:06 -0600

    v9

--- app/components/Layout.jsx
+++ app/components/Layout.jsx
@@ -13,7 +13,7 @@ export default function Layout({ children }) {
     <div className="min-h-screen flex flex-col">
       {/* Solo muestra el Header si NO es ruta admin */}
       {!isAdminRoute && <Header />}
-      <main className="flex-grow">{children}</main>
+      <main className="flex-grow container mx-auto px-4 sm:px-6 lg:px-8 shadow-xl">{children}</main>
       {/* Puedes ocultar el Footer en admin si quieres */}
       {!isAdminRoute && <Footer />}
     </div>
