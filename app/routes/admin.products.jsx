8f1df288d7b2d1a2780c4654392e73bbdfe3c5f5
MiriamHerrera <53792107+MiriamHerrera@users.noreply.github.com>
2025-05-18 01:10:51 -0600

    Create admin.products.jsx

---
+++ app/routes/admin.products.jsx
@@ -0,0 +1,9 @@
+export default function AdminProducts() {
+    return (
+      <div>
+        <h1 className="text-2xl font-bold mb-4">Products</h1>
+        <p>Aquí puedes gestionar los Products de los clientes.</p>
+        {/* Tabla de Products, filtros, detalles, etc. */}
+      </div>
+    );
+  }
